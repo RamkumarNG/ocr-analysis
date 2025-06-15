8bffe51f8065cfecdfdb6f3278837f5f71b9e50d
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-06-15 11:36:57 +0530

    added improvements

--- src/documents/urls.py
+++ src/documents/urls.py
@@ -2,11 +2,15 @@ from django.urls import re_path as url
 
 from rest_framework.routers import SimpleRouter
 
-from .views import DocumentViewSet, JobViewSet
+from .views import DocumentViewSet, JobViewSet, ping
 
 router = SimpleRouter()
+
+urlpatterns = [
+    url(r"^ping$", ping),
+]
 
 router.register(r'documents', DocumentViewSet, basename='documents')
 router.register(r'jobs', JobViewSet, basename='job')
 
-urlpatterns = router.urls
+urlpatterns += router.urls
