b9ea165c53d274d5524fbbdfb98e78300e233117
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-06-15 13:34:25 +0530

    created api for more structure freindly

--- src/app/settings.py
+++ src/app/settings.py
@@ -30,18 +30,26 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
 
+PROJECT_APPS = [
+    'api',
     'documents',
     'configs',
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",  # Framework for building Web APIs
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
